bd4cdb72e26bdcc37b2b95cf82fd05b408c2e2fc
AleksandrKamen <Kamen.alexandr@mail.ru>
2024-06-17 14:04:39 +0300

    add: init project

---
+++ AbolTask-core/src/main/java/org/demo/aboltaskcore/AbolTaskCoreApplication.java
@@ -0,0 +1,13 @@
+package org.demo.aboltaskcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbolTaskCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbolTaskCoreApplication.class, args);
+    }
+
+}
---
+++ AbolTask-core/src/test/java/org/demo/aboltaskcore/AbolTaskCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.aboltaskcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbolTaskCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
